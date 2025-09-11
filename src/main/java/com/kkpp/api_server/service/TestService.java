d1d18c4cce14821ea1acfbc1d9ef622ff60a431e
ChanHee Park <ch.park128@gmail.com>
2025-09-12 02:22:13 +0900

    테스트용 스켈레톤 코드 추가 (root, /hello)

---
+++ src/main/java/com/kkpp/api_server/service/TestService.java
@@ -0,0 +1,10 @@
+package com.kkpp.api_server.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+    public String hello() {
+        return "Hello! We are kkpp";
+    }
+}
